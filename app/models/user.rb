d89120b5370f818b17a6085fef5912fa1779e0ea
JohnHernCode <besthostandseoinfo@gmail.com>
2021-03-23 11:33:02 -0600

    Fix Linter Errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,26 +9,31 @@ class User < ApplicationRecord
   has_many :friendships_confirmation, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :friendships_confirmation
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  
+
   def friends
     friendships.map { |friendship| friendship.friend if friendship.confirmed }.compact
   end
+
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
+
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
+
   def confirm_friend(user)
     person = inverse_friendships.find { |friendship| friendship.user == user }
     person.confirmed = true
     person.save
     Friendship.create!(user_id: id, friend_id: user.id, confirmed: true)
   end
+
   def reject_friendship(user)
     friendship = inverse_friendships.find { |f| f.user == user }
     friendship.destroy
   end
+
   def friend?(user)
     friends.include?(user)
   end
