f73a6a809e6622b167421a91107ac8a86898c0b4
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-03-25 17:07:05 -0300

    hide/show friend request buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,8 @@
 module UsersHelper
   def show_friend_button(current_user, user)
     return if current_user == user
+    return if user.pending_friends.include?(current_user)
+    return if user.friend_requests.include?(current_user)
     
     button_to 'Send Friend request', "/users/#{current_user.id}/friendship/#{user.id}"
     
