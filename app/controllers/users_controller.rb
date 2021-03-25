e1863b0b746ce03cfd1433aad7f8a193c5d44f60
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-03-25 12:15:08 -0300

    create add Friendship button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def request_friendship
+    friend = User.find(params[:id])
+    friendship = Friendship.new(user_id: current_user.id, friend_id: friend.id, confirmed: false)
+    redirect_to root_path, notice: 'Friendship invite sent' if friendship.save
+  end
 end
