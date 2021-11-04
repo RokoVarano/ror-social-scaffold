76262af687fb1ee812d2ab8d8f256d1b7d873916
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-11-04 17:04:15 -0300

    REST API with no auth

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -2,8 +2,6 @@ module API
     module V1
       class Base < Grape::API
         mount API::V1::Users
-        mount API::V1::Posts
-        mount API::V1::Comments
       end
     end
   end
--- app/controllers/api/v1/users.rb
+++ app/controllers/api/v1/users.rb
@@ -12,13 +12,26 @@ module API
             requires :id, type: String, desc: "ID of the user"
           end
           get ":id" do
-            User.where(id: permitted_params[:id]).first!
+            puts(@current_user)
+            User.where(user_id: @current_user[:id]).first!
           end
           get ":id/posts" do
-            Post.where(user_id: request.path.split("/")[4])
+            Post.where(user_id: @current_user[:id]).sort
+          end
+          post ":id/posts" do
+            Post.create(user_id: @current_user[:id], content: params[:content])
+          end
+          get ":id/posts/:post_index" do
+            Post.where(user_id: @current_user[:id]).sort[request.path.split("/")[6].to_i - 1]
           end
           get ":id/posts/:post_id/comments" do
-            Comment.where(post_id: request.path.split("/")[6])
+            Comment.where(user_id: @current_user[:id]).where(post_id: request.path.split("/")[6]).sort
+          end
+          get ":id/posts/:post_id/comments" do
+            Comment.create(user_id: @current_user[:id], post_id: request.path.split("/")[6], content: params[:content])
+          end
+          get ":id/posts/:post_id/comments/:comment_index" do
+            Comment.where(user_id: @current_user[:id]).where(post_id: request.path.split("/")[6]).sort[request.path.split("/")[8].to_i - 1]
           end
         end
       end
