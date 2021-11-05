65a8ee259b954b9624897518d66d0e32547b2bb2
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-11-05 17:25:30 -0300

    api endpoints GET

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      respond_to :json
+      class Comment < ::Comment
+      end
+
+      def index
+        respond_with User.find(params[:user_id]).posts.sort[params[:post_id].to_i - 1].comments.sort
+      end
+
+      def show
+        respond_with User.find(params[:user_id]).posts.sort[params[:post_id].to_i - 1].comments.sort[params[:id].to_i - 1]
+      end
+    end
+  end
+end
---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      respond_to :json
+      class Post < ::Post
+      end
+
+      def index
+        respond_with Post.where(user_id: params[:user_id]).sort
+      end
+
+      def show
+        respond_with Post.where(user_id: params[:user_id]).sort[params[:id].to_i - 1]
+      end
+    end
+  end
+end
