ee425440d22296465a20b2f1f5edbf2e519d9a3e
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-11-10 22:37:48 +0300

    create api post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,10 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
 
     if @post.save
-      redirect_to posts_path, notice: 'Post was successfully created.'
+      respond_to do |format|
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.'}
+        format.json { render json: { message: "succesfully created." }, status: :ok }
+      end
     else
       timeline_posts
       render :index, alert: 'Post was not created.'
